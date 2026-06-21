038e3a912af32cb7ebedd7fddbe5cf7db49a3479
alishanepal000-ship-it <alishanepal000@gmail.com>
2026-06-21 21:35:47 +0545

    Arithmetic calcualtion and counter

---
+++ Self_Practice/arithmetic.py
@@ -0,0 +1,6 @@
+a = 20
+b = 6
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # true division — ALWAYS returns float
---
+++ Self_Practice/counter.py
@@ -0,0 +1,16 @@
+counter = 0
+
+counter += 1    # same as: counter = counter + 1
+print(counter)  # 1
+
+counter += 1
+print(counter)  # 2
+
+counter *= 10
+print(counter)  # 20
+
+counter //= 3
+print(counter)  # 6
+
+counter **= 2
+print(counter)  # 36
