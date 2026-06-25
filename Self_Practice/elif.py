b57e4e50c22c22c3508cf457b1e382cce732f220
alishanepal000-ship-it <alishanepal000@gmail.com>
2026-06-25 21:43:03 +0545

    I learned if-else statement

---
+++ Self_Practice/elif.py
@@ -0,0 +1,28 @@
+# # Set the initial age variable
+# age = 75
+
+# # 1. First check
+# if age < 13:
+#     print("You are a child.")
+
+# # 2. Second check (only evaluates if the 'if' was False)
+# elif age < 20:
+#     print("You are a teenager.")
+
+# # 3. Third check (only evaluates if both blocks above were False)
+# elif age < 65:
+#     print("You are an adult.")
+
+# # 4. Fallback option (runs if all above conditions fail)
+# else:
+#     print("You are a senior citizen.")
+
+letter = "B"
+if letter == "B":
+    print("letter is B")
+elif letter == "C":
+    print("letter is C")
+elif letter == "A":
+    print("letter is A")
+else:
+    print("letter isn't A, B or C")
